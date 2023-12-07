54b34f45a09686f9f715d72fb398dbfe283f588b
KharlosDanyell <oliveirakharlos@gmail.com>
2023-12-06 22:31:36 -0300

    webview

---
+++ App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { WebView } from 'react-native-webview';
+
+export default function App() {
+  return (
+    <WebView
+      source={{ uri: 'http://localhost:3001/login' }} 
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
